cbcd63839b8f8d2a38d4b9dcfbed1ec43055e644
Ivan Tang <ivanckstang@gmail.com>
2018-09-22 18:50:02 -0700

    working graphs

--- app/src/main/java/com/example/kylechan/plastickarma/HomeActivity.java
+++ app/src/main/java/com/example/kylechan/plastickarma/HomeActivity.java
@@ -7,6 +7,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
 
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
+
 public class HomeActivity extends AppCompatActivity {
 
     private TextView mTextMessage;
@@ -31,10 +35,25 @@ public class HomeActivity extends AppCompatActivity {
         }
     };
 
+    LineGraphSeries<DataPoint> series;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        //graph setup
+        double y,x;
+        x = -5.0;
+
+        GraphView graph = (GraphView) findViewById(R.id.graph);
+        series = new LineGraphSeries<DataPoint>();
+        for (int i = 0; i < 100; i++) {
+            x = x +0.1;
+            y = Math.sin(x);
+            series.appendData(new DataPoint(x, y), true, 500);
+        }
+        graph.addSeries(series);
 
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
